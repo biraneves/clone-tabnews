bcb4df97fdb5afd1ac6e8184e3370bb513b3e8f0
Ubirajara Neves <ubirajara.neves@usp.br>
2023-12-22 12:18:56 +0100

    Aplicados os estilos para a página inicial.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
 function Home() {
   return (
-    <div class="container">
-      <h1>Em breve</h1>
-      <p>algo legal aqui para você.</p>
-      <a href="https://github.com/biraneves">https://github.com/biraneves</a>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "500px",
+          height: "300px",
+          border: "solid 1px #bbb",
+          borderRadius: "5px",
+          backgroundColor: "#eee",
+          padding: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          fontFamily: "sans-serif",
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>Em breve</h1>
+        <p style={{ textAlign: "center", fontSize: "24px" }}>
+          algo legal aqui para você!
+        </p>
+        <p style={{ textAlign: "center" }}>
+          <a href="https://github.com/biraneves">
+            https://github.com/biraneves
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
