c041152627f94b0ee914667ad87c3f012c6c22da
Bira Neves <biraneves@outlook.com>
2025-03-23 08:30:25 +0100

    refactor: :recycle: extract code to functions with single responsibilities at status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,33 +3,37 @@ import database from "infra/database";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-
-  const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections",
-  );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
-
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenConnectionsResult = await database.query({
-    text: "SELECT count(*)::int from pg_stat_activity where datname = $1;",
-    values: [databaseName],
-  });
-  const databaseOpenConnectionsValue =
-    databaseOpenConnectionsResult.rows[0].count;
-
   await response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        open_connections: databaseOpenConnectionsValue,
+        version: await getDbVersionValue(database),
+        max_connections: await getDbMaxConnectionsValue(database),
+        open_connections: await getDbOpenConnectionsValue(database),
       },
     },
   });
 }
+
+const getDbVersionValue = async (db) => {
+  const databaseVersionResult = await db.query("SHOW server_version;");
+  return databaseVersionResult.rows[0].server_version;
+};
+
+const getDbMaxConnectionsValue = async (db) => {
+  const databaseMaxConnectionsResult = await db.query(
+    "SHOW max_connections",
+  );
+  return parseInt(databaseMaxConnectionsResult.rows[0].max_connections);
+};
+
+const getDbOpenConnectionsValue = async (db) => {
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await db.query({
+    text: "SELECT count(*)::int from pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+  return databaseOpenConnectionsResult.rows[0].count;
+};
 
 export default status;
